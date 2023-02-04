0c8f8e58ee6bbc6dba2e5034756db8e2740f4678
yeonkyoung <keep.ykk@gmail.com>
2023-02-05 03:06:38 +0900

    [#8, #9, #10, #11] 정상적으로 회원가입한 경우 /signin 경로로, 정상적으로 로그인한 경우 /todo 경로로, 로컬스토리지 토큰 여부에 따라 경로 리다이렉션 (#12)
    * feat: 회원가입 페이지에서 정상적으로 회원가입 완료시 리다이렉션
    
    * style: input css 수정 및 button css 추가
    
    * style: button css 수정
    
    * feat: 메인 페이지에서 버튼 선택으로 회원가입, 로그인 컴포넌트중 하나를 랜더링
    
    * feat: react-router-dom 설치, Router 컴포넌트 코드 작성
    
    * style: 메인 페이지 UI css 수정
    
    * feat: 로그인된다면 /todo 경로로 리다이렉트, 로컬스토리지에 엑세스토큰 저장
    
    * feat: todo Route 추가
    
    * feat: todo 컴포넌트 생성
    
    * style: todo css 생성
    
    * feat: 로컬스토리지 토큰이 있다면 todo 경로, 없다면 signin 경로로 리다이렉트

---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signin from './pages/signin';
+import Signup from './pages/signup';
+import Todo from './pages/todo';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

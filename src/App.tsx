4dc2a1dda1242280979bcbb405c2b6641391e839
yeonkyoung <keep.ykk@gmail.com>
2023-02-05 03:01:13 +0900

    feat: 로컬스토리지 토큰이 있다면 todo 경로, 없다면 signin 경로로 리다이렉트

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,61 @@ import React, { useState } from 'react';
 import './App.css';
 import Signin from './pages/signin';
 import Signup from './pages/signup';
+import Todo from './pages/todo';
+import { useNavigate } from 'react-router-dom';
 
 const SIGNUP_LABEL = '회원가입';
 const SIGNIN_LABEL = '로그인';
+const TODO_LABEL = 'TODO';
+const SIGNUP_TEXT = 'signup';
+const SIGNIN_TEXT = 'signin';
+const TODO_TEXT = 'todo';
+const SIGNUP_INDEX = 0;
+const SIGNIN_INDEX = 1;
+const TODO_INDEX = 2;
+const ROUTE_SIGNUP = '/signup';
+const ROUTE_SIGNIN = '/signin';
+const ROUTE_TODO = '/todo';
+const CLICKED_TEXT = 'clicked';
 
 function App() {
-  const [currentTab, setCurrentTab] = useState(true);
+  const [currentTab, setCurrentTab] = useState(SIGNIN_INDEX);
+  const localStorage = window.localStorage;
+  const navigate = useNavigate();
 
+  const checkLocalStorage = () => {
+    const access_token = localStorage.getItem('userInfo');
+    if (access_token?.length) {
+      return true;
+    }
+    return false;
+  };
   const onTabClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     const name = e.currentTarget.name;
-    setCurrentTab(name === 'signin' ? true : false);
+    if (name === SIGNUP_TEXT) {
+      if (checkLocalStorage()) {
+        navigate(ROUTE_TODO);
+        return;
+      }
+      setCurrentTab(SIGNUP_INDEX);
+      return;
+    }
+    if (name === SIGNIN_TEXT) {
+      if (checkLocalStorage()) {
+        navigate(ROUTE_TODO);
+        return;
+      }
+      setCurrentTab(SIGNIN_INDEX);
+      return;
+    }
+    if (name === TODO_TEXT) {
+      if (!checkLocalStorage()) {
+        navigate(ROUTE_SIGNIN);
+        return;
+      }
+      setCurrentTab(TODO_INDEX);
+      return;
+    }
   };
 
   return (
@@ -22,25 +67,42 @@ function App() {
           <li>
             <button
               onClick={onTabClick}
-              name="signup"
+              name={SIGNIN_TEXT}
               type="button"
-              className={`main-button ${currentTab ? '' : 'clicked'}`}
+              className={`main-button ${
+                currentTab === SIGNIN_INDEX && CLICKED_TEXT
+              }`}
             >
-              {SIGNUP_LABEL}
+              {SIGNIN_LABEL}
             </button>
           </li>
           <li>
             <button
               onClick={onTabClick}
-              name="signin"
+              name={TODO_TEXT}
               type="button"
-              className={`main-button ${currentTab ? 'clicked' : ''}`}
+              className={`main-button ${
+                currentTab === TODO_INDEX && CLICKED_TEXT
+              }`}
             >
-              {SIGNIN_LABEL}
+              {TODO_LABEL}
             </button>
           </li>
         </ul>
-        {currentTab ? <Signin /> : <Signup />}
+        {currentTab === SIGNUP_INDEX && <Signup />}
+        {currentTab === SIGNIN_INDEX && <Signin />}
+        {currentTab === TODO_INDEX && <Todo />}
+
+        <button
+          onClick={onTabClick}
+          name={SIGNUP_TEXT}
+          type="button"
+          className={`main-button ${
+            currentTab === SIGNUP_INDEX && CLICKED_TEXT
+          }`}
+        >
+          {SIGNUP_LABEL}
+        </button>
       </article>
     </div>
   );

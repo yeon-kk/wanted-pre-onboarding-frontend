9bd6bb0d1d263f59438b4f189a5727d816d693df
yeonkyoung <keep.ykk@gmail.com>
2023-02-05 15:21:03 +0900

    [#13, #14, #15, #16, #17] 투두리스트 입력, 수정, 삭제 UI 및 기능 구현
    * feat: TODO 리스트 입력 UI 구현
    
    * feat TODO의 체크박스 구현
    
    * feat: 수정 버튼, 삭제 버튼 UI 구현
    
    * feat: 투두 삭제 기능 구현
    
    * feat: 투두 리스트의 수정 기능 구현
    
    * srtle: 투두 리스트 css 추가
    
    * chore: 하드코딩된 텍스트를 상수화

--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -1,7 +1,151 @@
+import { useState } from 'react';
 import './todo.css';
 
+const TODO_TITLE = 'TODO LIST';
+const ADD_LABEL = '추가';
+const MODIFY_LABEL = '수정';
+const DELETE_LABEL = '삭제';
+const SUBMIT_TEXT = '제출';
+const CANCEL_TEXT = '취소';
+
+interface todoType {
+  todo: string;
+  modify: boolean;
+}
+
 function Todo() {
-  return <div className="login-layout"></div>;
+  const [todoList, setTodoList] = useState<todoType[]>([]);
+  const [createTodo, setCreateTodo] = useState('');
+  const [modifyTodo, setModifyTodo] = useState('');
+
+  const handleCreateTodoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCreateTodo(e.currentTarget.value);
+  };
+
+  const handleModifyTodoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setModifyTodo(e.currentTarget.value);
+  };
+
+  const handleAddButtonClick = () => {
+    const tmpList = [...todoList];
+    tmpList.push({ todo: createTodo, modify: false });
+    setTodoList([...tmpList]);
+    setCreateTodo('');
+  };
+
+  const handleDeleteClick = (index: number) => () => {
+    const tmpList = [...todoList];
+    tmpList.splice(index, 1);
+    setTodoList([...tmpList]);
+  };
+
+  const handleModifyClick = (todo: string, index: number) => () => {
+    const tmpList = [...todoList];
+    const modified = {
+      todo: todo,
+      modify: true,
+    };
+    tmpList.splice(index, 1, modified);
+    setTodoList([...tmpList]);
+    setModifyTodo(todo);
+  };
+
+  const handleModifySbumitClick = (index: number, value: string) => () => {
+    const tmpList = [...todoList];
+    const modified = {
+      todo: value,
+      modify: false,
+    };
+    tmpList.splice(index, 1, modified);
+    setTodoList([...tmpList]);
+    setModifyTodo('');
+  };
+
+  const handleCancleClick = (index: number) => () => {
+    const tmpList = [...todoList];
+    const modified = {
+      todo: tmpList[index].todo,
+      modify: false,
+    };
+    tmpList.splice(index, 1, modified);
+    setTodoList([...tmpList]);
+  };
+
+  return (
+    <div className="login-layout">
+      <h4>{TODO_TITLE}</h4>
+      <div className="input-container">
+        <input
+          value={createTodo}
+          className="todo-input"
+          onChange={handleCreateTodoChange}
+          data-testid="new-todo-input"
+        />
+        <button
+          className="todo-button"
+          data-testid="new-todo-add-button"
+          type="button"
+          onClick={handleAddButtonClick}
+        >
+          {ADD_LABEL}
+        </button>
+      </div>
+      <ol className="todo-list-container">
+        {todoList.map(({ todo, modify }, index) => (
+          <li className="todo-container">
+            <input type="checkbox" />
+            {modify ? (
+              <label className="todo-label">
+                <input
+                  className="todo-input"
+                  value={modifyTodo}
+                  onChange={handleModifyTodoChange}
+                ></input>
+                <div className="todo-button-container">
+                  <button
+                    type="button"
+                    className="todo-button"
+                    onClick={handleModifySbumitClick(index, modifyTodo)}
+                  >
+                    {SUBMIT_TEXT}
+                  </button>
+                  <button
+                    className="todo-button"
+                    type="button"
+                    onClick={handleCancleClick(index)}
+                  >
+                    {CANCEL_TEXT}
+                  </button>
+                </div>
+              </label>
+            ) : (
+              <label className="todo-label">
+                <div className="todo-content">{todo}</div>
+                <div className="todo-button-container">
+                  <button
+                    className="todo-button"
+                    type="button"
+                    data-testid="modify-button"
+                    onClick={handleModifyClick(todo, index)}
+                  >
+                    {MODIFY_LABEL}
+                  </button>
+                  <button
+                    className="todo-button"
+                    type="button"
+                    data-testid="delete-button"
+                    onClick={handleDeleteClick(index)}
+                  >
+                    {DELETE_LABEL}
+                  </button>
+                </div>
+              </label>
+            )}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
 
 export default Todo;

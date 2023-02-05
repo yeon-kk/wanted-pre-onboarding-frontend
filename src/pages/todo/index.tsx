a1016520dfb2bc656713231ef1eb45af3af1a2c4
yeonkyoung <keep.ykk@gmail.com>
2023-02-05 18:05:20 +0900

    [#19, #20, #21, #22] TODO리스트에 createTodo, getTodos, updateTodo, deleteTodo API 연결 (#23)
    * feat: 투두리스트 getTodos API 연결, 투두리스트 데이터타입 수정
    
    * feat: 투두리스트 createTodo API 연결
    
    * feat: 투두리스트 updateTodo API 연결
    
    * feat: 투두리스트 deleteTodo API 연결

--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './todo.css';
 
 const TODO_TITLE = 'TODO LIST';
@@ -7,16 +7,89 @@ const MODIFY_LABEL = '수정';
 const DELETE_LABEL = '삭제';
 const SUBMIT_TEXT = '제출';
 const CANCEL_TEXT = '취소';
+const TODOLIST_URL = 'https://pre-onboarding-selection-task.shop/todos';
 
 interface todoType {
+  id: number;
   todo: string;
-  modify: boolean;
+  isCompleted: boolean;
+  userId: number;
 }
 
 function Todo() {
   const [todoList, setTodoList] = useState<todoType[]>([]);
   const [createTodo, setCreateTodo] = useState('');
-  const [modifyTodo, setModifyTodo] = useState('');
+  const [modifyTodo, setModifyTodo] = useState<string>('');
+  const [modifyList, setModifyList] = useState<boolean[]>([]);
+  const localStorage = window.localStorage;
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch(TODOLIST_URL, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('userInfo')}`,
+        },
+      });
+      return await response.json();
+    } catch (error) {
+      console.log(error);
+    }
+    return;
+  };
+
+  const postCreateTodo = async (data = {}) => {
+    try {
+      const response = await fetch(TODOLIST_URL, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('userInfo')}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+      console.log(result);
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+    return;
+  };
+
+  const putUpdateTodo = async (id: number, data = {}) => {
+    try {
+      const response = await fetch(`${TODOLIST_URL}/${id}`, {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('userInfo')}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+      console.log(result);
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+    return;
+  };
+
+  const deleteTodo = async (id: number) => {
+    try {
+      await fetch(`${TODOLIST_URL}/${id}`, {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('userInfo')}`,
+          'Content-Type': 'application/json',
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    return;
+  };
 
   const handleCreateTodoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCreateTodo(e.currentTarget.value);
@@ -28,48 +101,71 @@ function Todo() {
 
   const handleAddButtonClick = () => {
     const tmpList = [...todoList];
-    tmpList.push({ todo: createTodo, modify: false });
+    tmpList.push({
+      id: tmpList.length + 1,
+      todo: createTodo,
+      isCompleted: false,
+      userId: 1,
+    });
+    const tmpModify = [...modifyList];
+    tmpModify.push(false);
+    setModifyList([...tmpModify]);
     setTodoList([...tmpList]);
+    postCreateTodo({ todo: createTodo });
     setCreateTodo('');
   };
 
   const handleDeleteClick = (index: number) => () => {
     const tmpList = [...todoList];
+    const id = tmpList[index].id;
     tmpList.splice(index, 1);
     setTodoList([...tmpList]);
+    deleteTodo(id);
   };
 
   const handleModifyClick = (todo: string, index: number) => () => {
-    const tmpList = [...todoList];
-    const modified = {
-      todo: todo,
-      modify: true,
-    };
-    tmpList.splice(index, 1, modified);
-    setTodoList([...tmpList]);
+    const tmpModify = [...modifyList];
+    tmpModify[index] = true;
+    setModifyList([...tmpModify]);
     setModifyTodo(todo);
   };
 
-  const handleModifySbumitClick = (index: number, value: string) => () => {
-    const tmpList = [...todoList];
-    const modified = {
-      todo: value,
-      modify: false,
+  const handleModifySbumitClick =
+    (index: number, todo: string, isCompleted: boolean) => () => {
+      const tmpList = [...todoList];
+      const modified = {
+        ...tmpList[index],
+        todo,
+        isCompleted,
+      };
+      tmpList.splice(index, 1, modified);
+      setTodoList([...tmpList]);
+      const tmpModify = [...modifyList];
+      tmpModify[index] = false;
+      setModifyList([...tmpModify]);
+      setModifyTodo('');
+      putUpdateTodo(modified.id, { todo, isCompleted });
     };
-    tmpList.splice(index, 1, modified);
-    setTodoList([...tmpList]);
+
+  const handleCancleClick = (index: number) => () => {
+    if (modifyList) {
+      const tmpModify = [...modifyList];
+      tmpModify[index] = true;
+      setModifyList([...tmpModify]);
+    }
     setModifyTodo('');
   };
 
-  const handleCancleClick = (index: number) => () => {
-    const tmpList = [...todoList];
-    const modified = {
-      todo: tmpList[index].todo,
-      modify: false,
-    };
-    tmpList.splice(index, 1, modified);
-    setTodoList([...tmpList]);
+  const initTodos = async () => {
+    const todos = await getTodos();
+    setTodoList([...todos]);
+    const arr = todos.map(() => false);
+    setModifyList([...arr]);
   };
+
+  useEffect(() => {
+    initTodos();
+  }, []);
 
   return (
     <div className="login-layout">
@@ -91,10 +187,10 @@ function Todo() {
         </button>
       </div>
       <ol className="todo-list-container">
-        {todoList.map(({ todo, modify }, index) => (
+        {todoList.map(({ todo }, index) => (
           <li className="todo-container">
             <input type="checkbox" />
-            {modify ? (
+            {modifyList && modifyList[index] ? (
               <label className="todo-label">
                 <input
                   className="todo-input"
@@ -105,7 +201,7 @@ function Todo() {
                   <button
                     type="button"
                     className="todo-button"
-                    onClick={handleModifySbumitClick(index, modifyTodo)}
+                    onClick={handleModifySbumitClick(index, modifyTodo, true)}
                   >
                     {SUBMIT_TEXT}
                   </button>
